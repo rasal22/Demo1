a7b6b1106f7e914641fa44588437632776ca2cd9
rasal20 <rasalrichu96@gmail.com>
2023-07-17 14:20:48 +0400

    added

---
+++ passvlproject/passvlapp/views.py
@@ -0,0 +1,11 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+#Create your views here.
+
+def demo(request):
+    name='india'
+    return render(request,"index.html", {'obj':name})
+
+
+
